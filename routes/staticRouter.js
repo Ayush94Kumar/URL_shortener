a3bd7111dd43fdfd55fc1ad8693c8079b0827c6e
ayush76671kumar@gmail.com <ayushkumar94703@gmail.com>
2026-05-15 21:44:56 +0530

    Fixing bugs

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,11 +1,12 @@
 const express =require('express');
-
+const URL = require('../model/url');
 const router =express.Router();
 
 router.get("/", async (req,res)=>{
     const allurls =await URL.find({})
     return res.render("home", {
-        urls :allurls
+        urls :allurls,
+        id: req.query.id // This grabs the ID from the URL if it exists
     });
 })
 
